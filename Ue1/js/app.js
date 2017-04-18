32bffc4eb5f9a78e904aae3868ad035f53fda048
julius <julius@serienjunkies.de>
2017-04-18 13:00:33 +0200

    first commit

--- Ue1/js/app.js
+++ Ue1/js/app.js
@@ -18,3 +18,30 @@ function xxxxx() {
         document.getElementById("xxxx").innerHTML = "text-test";
     }
 }
+
+window.onload = function () {
+    var picture_frames = document.getElementsByClassName("picture-frame");
+    var picture_button_frames = document.getElementsByClassName("picture-button-frame");
+    var like_buttons = document.getElementsByClassName("like-button");
+
+    for (var i = 0; i < picture_frames.length; i++) {
+        (function() {
+            picture_frames[i].addEventListener("mouseover", showButtons(picture_button_frames[i]), false);
+            picture_frames[i].addEventListener("mouseout", hideButtons(picture_button_frames[i]), false);
+            like_buttons[i].addEventListener("click", pressLikeButton(like_buttons[i]), false);
+        })()
+    }
+
+}
+
+function pressLikeButton(like_button) {
+    like_button.style.display = "Gef&auml;llt dir";
+}
+
+function showButtons(button_frame) {
+    button_frame.style.display = "block";
+}
+
+function hideButtons(button_frame) {
+    button_frame.style.display = "none";
+}
