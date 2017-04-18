e0299c2a82bace5fbb73417b801f17554081660c
Stephan <duenkel.stephan@gmail.com>
2017-04-18 20:55:04 +0200

    Hotfix - html-layout update, julius-stephan code merged - deleted image.css from julius -> putted into app.css

--- Ue1/js/app.js
+++ Ue1/js/app.js
@@ -1,20 +1,55 @@
-var addFunctionOnWindowLoad = function (callback) {
-    if (window.addEventListener) {
-        window.addEventListener('load', callback, false);
-    } else {
-        window.attachEvent('onload', callback);
+window.addEventListener("load", function (event) {
+    
+    var pics = document.getElementsByTagName("img");
+    var picture_button_frames = document.getElementsByClassName("picture-button-frame");
+    var like_buttons = document.getElementsByClassName("like-button");
+    var detail_buttons = document.getElementsByClassName("detail-button");
+    
+    for (var i = 0; i < pics.length; i++) {
+        var pic = pics[i];
+        
+        // … find button objects and add listener …
+        pic.addEventListener("mouseover", function (event) {
+            picture_button_frames.style.display = "block";
+            alert("asdasd");
+        });
+        likeButton.addEventListener("click", function (event) {
+            like_buttons.style.display = "Gef&auml;llt dir";
+        });
+        detailButton.addEventListener("click", function (event) {
+            detail_buttons.style.display = "Gef&auml;llt dir";
+        });
     }
-};
+});
 
-addFunctionOnWindowLoad(xxx);
 
-function xxx() {
-    document.getElementById("XXX").onmouseover = xxxxx;
+/**
+window.onload = function () {
+    var picture_frames = document.getElementsByClassName("picture-frame");
+    var picture_button_frames = document.getElementsByClassName("picture-button-frame");
+    var like_buttons = document.getElementsByClassName("like-button");
+
+    for (var i = 0; i < picture_frames.length; i++) {
+        var pic = picture_frames[i];
+        
+        (function() {
+            picture_frames[i].addEventListener("mouseover", showButtons(pic), false);
+            picture_frames[i].addEventListener("mouseout", hideButtons(pic), false);
+            like_buttons[i].addEventListener("click", pressLikeButton(pic), false);
+        })()
+    }
+
 }
 
-function xxxxx() {
-    if (xxx.selectedIndex == 0) {
-        document.getElementById("xxxx").style.display = "inline";
-        document.getElementById("xxxx").innerHTML = "text-test2";
-    }
+function pressLikeButton(like_button) {
+    like_button.style.display = "Gef&auml;llt dir";
 }
+
+function showButtons(button_frame) {
+    button_frame.style.display = "block";
+}
+
+function hideButtons(button_frame) {
+    button_frame.style.display = "none";
+}
+**/
